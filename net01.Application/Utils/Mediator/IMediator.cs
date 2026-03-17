2762adf53c262b0a76428a4b71bd7367b2ffd630
Nayitdesaily <105016440+Nayitdesaily@users.noreply.github.com>
2026-03-17 10:07:04 -0500

    Add files via upload

---
+++ net01.Application/Utils/Mediator/IMediator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net01.Application.Utils.Mediator
+{
+    public interface IMediator
+    {
+        Task<TResponse> Send<TResponse>(IRequest<TResponse> request);
+        
+    }
+}
---
+++ net01.Application/Utils/Mediator/Mediator.cs
@@ -0,0 +1,45 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using net01.Application.Exceptions;
+
+namespace net01.Application.Utils.Mediator
+{
+    public class Mediator : IMediator
+    {
+        private readonly IServiceProvider serviceProvider;
+        public Mediator(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+        public async Task<TResponse> Send<TResponse>(IRequest<TResponse> request)
+        {
+            var validatorType = typeof(IValidator<>).MakeGenericType(request.GetType());
+
+            var validator = serviceProvider.GetService(validatorType);
+
+            if (validator is not null)
+            {
+                var validatorMethod = validatorType.GetMethod("ValidateAsync");
+                var validate = (Task)validatorMethod!.Invoke(validator, new object[] {request, CancellationToken.None })!;
+                await validate.ConfigureAwait(false);
+                var result = validate.GetType().GetProperty("Result");
+                var validationResult = (ValidationResult)result!.GetValue(validate)!;
+                if (!validationResult.IsValid)
+                {
+                    throw new Exceptions.ValidationException(validationResult);
+                }
+            }
+
+            var useCaseType = typeof(IRequestHandler<,>).MakeGenericType(request.GetType(), typeof(TResponse));
+            var useCase = serviceProvider.GetService(useCaseType);
+
+            if(useCase is null)
+            {
+                throw new MediatorException($"No se encontro un handler para {request.GetType().Name}");
+            }
+
+            var method = useCaseType.GetMethod("Handle");
+            return await (Task<TResponse>)method.Invoke(useCase, new object[] { request });
+        }
+    }
+}
